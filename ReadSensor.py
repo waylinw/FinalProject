0bbdbe1a6ac698851c4700d9fe7f5f9c3b06b7e3
waylinw <waylinw@yahoo.com>
2016-11-16 16:54:03 -0800

    Added in basic code to print out reading every 2 seconds

--- ReadSensor.py
+++ ReadSensor.py
@@ -1,3 +1,13 @@
+GPIO_PORT = 18
+SENSOR = Adafruit_DHT.DHT11
+
 import sys
+import time
 import Adafruit_DHT
+
+while True:
+    humidity, temperature = Adafruit_DHT.read_retry(SENSOR, GPIO_PORT)
+    temperature *= 9/5.0 + 32
+    print("Temperature: ", temperature, " deg F, Humidity: ", humidity, " %")
+    time.sleep(2)
 
