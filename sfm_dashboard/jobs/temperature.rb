4f018296c61df65159094c5c020fac09c969670d
Peter Georgiev <georgiev2592@gmail.com>
2016-11-22 17:16:49 -0800

    updated temperature widget

--- sfm_dashboard/jobs/temperature.rb
+++ sfm_dashboard/jobs/temperature.rb
@@ -1,4 +1,23 @@
-# :first_in sets how long it takes before the job is first run. In this case, it is run immediately
-SCHEDULER.every '1m', :first_in => 0 do |job|
-  send_event('widget_id', { })
+require 'mysql2'
+
+points = []
+
+SCHEDULER.every '2s', :first_in => 0 do |job|
+
+  # mysql connection
+  db = Mysql2::Client.new(host: "localhost", username: "root", password: "root", database: "sfm")
+
+  # mysql query
+  sql = "SELECT * FROM Data LIMIT 1"
+
+  # execute the query
+  results = db.query(sql)
+  
+  # sending to List widget, so map to :label and :value
+  results.map do |row|
+  	points.shift
+  	points << {x: row['created_at'], y: row['temp_c']}
+  end
+
+  send_event('temperature', points: points)
 end
